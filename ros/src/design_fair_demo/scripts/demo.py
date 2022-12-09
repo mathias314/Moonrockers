2f25cec0eb249297a4cc64992da841df5e866c4c
Mathew Clutter <mathew.clutter@mines.sdsmt.edu>
2022-12-09 23:04:26 +0000

    Resolve "Drive robot from point to hopper using Aruco tag"

--- ros/src/design_fair_demo/scripts/demo.py
+++ ros/src/design_fair_demo/scripts/demo.py
@@ -11,41 +11,45 @@ left_power = None
 right_power = None
 autoDriveLeft = None
 autoDriveRight = None
-targetDist = 2
+targetDist = 1
 deadzone = 0.1
 rotationDeadzone = 0.2
 lastTargetDistTime = 0
-autodriveTimeout = 1 # seconds
+autodriveTimeout = 1  # seconds
+
 
 def poseCallback(data):
-  global targetDist
-  global lastTargetDistTime
+    global targetDist
+    global lastTargetDistTime
 
-  if (time.time() - lastTargetDistTime < autodriveTimeout):
-    Tvec = (data.position.x, data.position.y, data.position.z)
-    dist = math.sqrt(Tvec[0]**2 + Tvec[1]**2 + Tvec[2]**2)
+    # if (time.time() - lastTargetDistTime < autodriveTimeout):
+    if True:
+        Tvec = (data.position.x, data.position.y, data.position.z)
+        dist = math.sqrt(Tvec[0] ** 2 + Tvec[1] ** 2 + Tvec[2] ** 2)
 
-    if (Tvec[0] > rotationDeadzone):
-      autoDriveLeft.publish(-0.1)
-      autoDriveRight.publish(0.1)
-    elif (Tvec[0] < -rotationDeadzone):
-      autoDriveLeft.publish(0.1)
-      autoDriveRight.publish(-0.1)
-    elif (dist > targetDist + deadzone):
-      autoDriveLeft.publish(0.1)
-      autoDriveRight.publish(0.1)
-    elif (dist < targetDist - deadzone):
-      autoDriveLeft.publish(-0.1)
-      autoDriveRight.publish(-0.1)
-    else:
-      autoDriveLeft.publish(0)
-      autoDriveRight.publish(0)
+        if Tvec[0] > rotationDeadzone:
+            autoDriveLeft.publish(-0.1)
+            autoDriveRight.publish(0.1)
+        elif Tvec[0] < -rotationDeadzone:
+            autoDriveLeft.publish(0.1)
+            autoDriveRight.publish(-0.1)
+        elif dist > targetDist + deadzone:
+            autoDriveLeft.publish(0.1)
+            autoDriveRight.publish(0.1)
+        elif dist < targetDist - deadzone:
+            autoDriveLeft.publish(-0.1)
+            autoDriveRight.publish(-0.1)
+        else:
+            autoDriveLeft.publish(0)
+            autoDriveRight.publish(0)
+
 
 def targetDistanceCallback(data):
-  global targetDist
-  global lastTargetDistTime
-  lastTargetDistTime = time.time()
-  targetDist = data.data
+    global targetDist
+    global lastTargetDistTime
+    lastTargetDistTime = time.time()
+    targetDist = data.data
+
 
 def demo():
     # initialize ROS stuff
@@ -53,18 +57,20 @@ def demo():
     global right_power
     global autoDriveLeft
     global autoDriveRight
-    rospy.init_node('demo', anonymous=True)
-    autoDriveLeft = rospy.Publisher('autoDriveLeft', Float32, queue_size=100)
-    autoDriveRight = rospy.Publisher('autoDriveRight', Float32, queue_size=100)
-    rospy.Subscriber('pose', Pose, poseCallback)   
-    rospy.Subscriber('targetDistance', Float32, targetDistanceCallback)   
+    rospy.init_node("demo", anonymous=True)
+    autoDriveLeft = rospy.Publisher("autoDriveLeft", Float32, queue_size=100)
+    autoDriveRight = rospy.Publisher("autoDriveRight", Float32, queue_size=100)
+    rospy.Subscriber("pose", Pose, poseCallback)
+    rospy.Subscriber("targetDistance", Float32, targetDistanceCallback)
     rate = rospy.Rate(20)
 
     while not rospy.is_shutdown():
-        rate.sleep()  
+        rate.sleep()
 
-if __name__ == '__main__':
-  try:
-    demo()
-  except rospy.ROSInterruptException:
-    pass
+
+if __name__ == "__main__":
+    try:
+        demo()
+    except rospy.ROSInterruptException:
+        print("exception caught in vision demo file")
+        pass
