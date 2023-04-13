e174d52be5505f65a4f129422431a36b864f286b
Mathew Clutter <mathew.clutter@mines.sdsmt.edu>
2023-04-13 04:26:22 +0000

    Resolve "Get Kinect Working within the ROS Setup on the Bot"

--- kinectPlaying/obstacleDetect.py
+++ kinectPlaying/obstacleDetect.py
@@ -19,94 +19,115 @@ import time
 import sys
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import subprocess
 
 VISUALIZE = True
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Please enter yaml file to convert to image!")
-        exit(1)
+    while True:
+        print("in main")
+        fileName = "frame.yaml"
 
-    fileName = sys.argv[1]
-    # Load the YAML data from the file
-    with open(fileName, "r") as f:
-        fileName = fileName[:-4]
+        cmd = "rostopic echo /depth/image_raw -n 1 > " + fileName
 
-        data = yaml.safe_load(f)
-        array = data["data"]
+        while True:
+            print("trying to output subprocess")
+            result = subprocess.run(cmd, shell=True, check=True)
+            print("attempted, output" + str(result))
+            if result.returncode == 0:
+                print("rostopic echoing complete!")
+                break
+            else:
+                print("unsuccessful, trying again to get rostopic output")
 
-        print("Finished loading in yaml, now to process image!")
-        start = time.time()
+        # Load the YAML data from the file
+        with open(fileName, "r") as f:
+            fileName = fileName[:-5]
 
-        packed = struct.pack(
-            "<" + "H" * (len(array) // 2),
-            *[array[i] + (array[i + 1] << 8) for i in range(0, len(array), 2)]
-        )
+            print("started loading in yaml")
 
-        merged = struct.unpack("<" + "H" * (len(array) // 2), packed)
-        # Merge the raw data from the rostopic output in little endian fashion to make a 16 bit value
-        # Merges the 2 8 bit values into 1 16 bit output
+            data = f.read()
+            print(type(data))
+            dataStripped = data.replace("-", "")
+            print("stripping done")
 
-        rowCounter = 0
-        colCounter = 0
-        imageMatrix = np.zeros((1024, 1024), dtype=np.uint16)
-        picDict = defaultdict(list)
-        for val in merged:
-            imageMatrix[rowCounter][colCounter] = val
-            picDict["x"].append(rowCounter)
-            picDict["y"].append(colCounter)
-            picDict["val"].append(val)
+            yamlData = yaml.safe_load(dataStripped)
+            array = yamlData["data"]
 
-            colCounter += 1
-            if colCounter == 1024:
-                rowCounter += 1
-                colCounter = 0
+            print("Finished loading in yaml, now to process image!")
+            start = time.time()
 
-        df = pd.DataFrame(picDict)
-        # pd.DataFrame.to_csv(df, fileName + ".csv")
-        image = (imageMatrix).astype(
-            np.uint16
-        )  # To switch to 16 bit image, remove divide by 256 and change type to uint16
+            packed = struct.pack(
+                "<" + "H" * (len(array) // 2),
+                *[array[i] + (array[i + 1] << 8) for i in range(0, len(array), 2)]
+            )
 
-        cv2.imwrite(fileName + ".tiff", image)
+            merged = struct.unpack("<" + "H" * (len(array) // 2), packed)
+            # Merge the raw data from the rostopic output in little endian fashion to make a 16 bit value
+            # Merges the 2 8 bit values into 1 16 bit output
 
-        image = cv2.imread(fileName + ".tiff", cv2.IMREAD_GRAYSCALE)
+            rowCounter = 0
+            colCounter = 0
+            imageMatrix = np.zeros((1024, 1024), dtype=np.uint16)
+            picDict = defaultdict(list)
+            for val in merged:
+                imageMatrix[rowCounter][colCounter] = val
+                picDict["x"].append(rowCounter)
+                picDict["y"].append(colCounter)
+                picDict["val"].append(val)
 
-        # Apply a Gaussian filter to the image to remove noise
-        image = cv2.GaussianBlur(image, (3, 3), 1)
+                colCounter += 1
+                if colCounter == 1024:
+                    rowCounter += 1
+                    colCounter = 0
 
-        # Take the first derivative of the image in the x direction
-        dx = cv2.Sobel(image, cv2.CV_32F, 1, 0, 3)
+            df = pd.DataFrame(picDict)
+            # pd.DataFrame.to_csv(df, fileName + ".csv")
+            image = (imageMatrix / 256).astype(
+                np.uint8
+            )  # To switch to 16 bit image, remove divide by 256 and change type to uint16
 
-        # Take the second derivative of the image in the x direction
-        dxx = cv2.Sobel(dx, cv2.CV_32F, 1, 0, 3)
+            cv2.imwrite(fileName + ".png", image)
 
-        # Save the image to disk
-        cv2.imwrite(fileName + "dxx.png", dxx)
+            image = cv2.imread(fileName + ".png", cv2.IMREAD_GRAYSCALE)
 
-        derivativeArray = cv2.imread(fileName + "dxx.png", cv2.IMREAD_GRAYSCALE)
-        objectMatrix = np.zeros((1024, 1024), dtype=np.uint8)
-        objectDict = defaultdict(list)
-        for i, row in enumerate(derivativeArray):
-            for j, entry in enumerate(row):
-                if entry != 0 and imageMatrix[i][j] != 0:
-                    objectMatrix[i][j] = imageMatrix[i][j]
-                    objectDict["x"].append(i)
-                    objectDict["y"].append(j)
-                    objectDict["distance"].append(imageMatrix[i][j])
+            # Apply a Gaussian filter to the image to remove noise
+            image = cv2.GaussianBlur(image, (3, 3), 1)
 
-        # totalImageDataFrame = pd.DataFrame(objectMatrix)
-        # pd.DataFrame.to_csv(totalImageDataFrame, fileName + "Derivative.csv")
+            # Take the first derivative of the image in the x direction
+            dx = cv2.Sobel(image, cv2.CV_32F, 1, 0, 3)
 
-        obstacleList = pd.DataFrame(objectDict)
-        pd.DataFrame.to_csv(obstacleList, fileName + "ObstacleList.csv")
+            # Take the second derivative of the image in the x direction
+            dxx = cv2.Sobel(dx, cv2.CV_32F, 1, 0, 3)
 
-        finish = time.time() - start
-        print("Time to finish was " + str(finish))
+            # Save the image to disk
+            cv2.imwrite(fileName + "dxx.png", dxx)
 
-        if VISUALIZE:
-            createVisualization(fileName + "ObstacleList.csv")
+            derivativeArray = cv2.imread(fileName + "dxx.png", cv2.IMREAD_GRAYSCALE)
+            objectMatrix = np.zeros((1024, 1024), dtype=np.uint8)
+            objectDict = defaultdict(list)
+            for i, row in enumerate(derivativeArray):
+                for j, entry in enumerate(row):
+                    if entry != 0 and imageMatrix[i][j] != 0:
+                        objectMatrix[i][j] = imageMatrix[i][j]
+                        objectDict["x"].append(i)
+                        objectDict["y"].append(j)
+                        objectDict["distance"].append(imageMatrix[i][j])
+
+            # totalImageDataFrame = pd.DataFrame(objectMatrix)
+            # pd.DataFrame.to_csv(totalImageDataFrame, fileName + "Derivative.csv")
+
+            obstacleList = pd.DataFrame(objectDict)
+            pd.DataFrame.to_csv(obstacleList, fileName + "ObstacleList.csv")
+
+            finish = time.time() - start
+            print("Time to finish was " + str(finish))
+
+            if VISUALIZE:
+                createVisualization(fileName + "ObstacleList.csv")
+
+        print("iteration done!")
 
 
 def createVisualization(fileName: str) -> None:
@@ -128,7 +149,7 @@ def createVisualization(fileName: str) -> None:
     ax.set_zlabel("Distance")
 
     # show the plot
-    plt.show()
+    plt.show()  # block=False qwarg doesn't wanna work
 
 
 if __name__ == "__main__":
